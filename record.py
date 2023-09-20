87498fa9989f6cbf756b4df23a5eec942ba88b1b
giovannav <giovannafrederico60@gmail.com>
2023-09-20 16:34:49 -0300

    recordv video

--- record.py
+++ record.py
@@ -5,7 +5,7 @@ cap= cv2.VideoCapture(0)
 width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+writer= cv2.VideoWriter('thermalvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
 
 while True:
     ret, frame= cap.read()
@@ -16,7 +16,6 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
-
 
 cap.release()
 writer.release()
