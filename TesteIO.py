59caf034ef3d581f71c29c6f763a6c078116faf2
giovannav <giovannafrederico60@gmail.com>
2023-09-20 16:32:22 -0300

    recordv video

--- TesteIO.py
+++ TesteIO.py
@@ -16,31 +16,31 @@ while(True):
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
         # configura câmeras
-        cap0 = cv2.VideoCapture(0)
+        #cap0 = cv2.VideoCapture(0)
         cap1 = cv2.VideoCapture(1)
 
         # lê captura imagens das câmeras
-        _, frame0 = cap0.read()
+        #_, frame0 = cap0.read()
         _, frame1 = cap1.read()
 
         # rotate
-        frame0 = cv2.rotate(frame0, cv2.ROTATE_180) # camera termica
-        #frame1 = cv2.rotate(frame1, cv2.ROTATE_90_CLOCKWISE) # camera rgb
+        #frame0 = cv2.rotate(frame0, cv2.ROTATE_180) # camera termica
+        frame1 = cv2.rotate(frame1, cv2.ROTATE_90_CLOCKWISE) # camera rgb
 
         # resize
-        img0 = cv2.resize(frame0, (300, 300), interpolation=cv2.INTER_AREA)
+        #img0 = cv2.resize(frame0, (300, 300), interpolation=cv2.INTER_AREA)
         img1 = cv2.resize(frame1, (300, 300), interpolation=cv2.INTER_AREA)
 
         # renomeia imagens
-        name0 = 'robot_images/camera0_' + timestamp + '.jpg'
+        #name0 = 'robot_images/camera0_' + timestamp + '.jpg'
         name1 = 'robot_images/camera1_' + timestamp + '.jpg'
 
         # salva imagens
-        cv2.imwrite(name0, img0)
+        #cv2.imwrite(name0, img0)
         cv2.imwrite(name1, img1)
 
         # fecha conexão com a câmera
-        cap0.release()
+        #cap0.release()
         cap1.release()
         cv2.destroyAllWindows()
         
